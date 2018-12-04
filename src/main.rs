b630ef30543f607de7d8d37be4dde1035c015194
dominik-ernst <dominik.ernst@tu-berlin.de>
2018-12-04 10:35:22 +0100

    days 1 to 3

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod days;
+extern crate regex;
+
+fn main() {
+    println!("Day 1 Results: {:?}", days::day1());
+    println!("Day 2 Results: {:?}", days::day2());
+    println!("Day 3 Results: {:?}", days::day3());
+}
